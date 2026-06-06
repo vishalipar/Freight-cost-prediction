e9c9bed3372ae5647957a1bab5910f9143df7c08
vishalipar <vishalipar04@gmail.com>
2026-06-06 13:25:52 +0530

    correct the score spelling

--- invoice_flagging/modeling_evaluation.py
+++ invoice_flagging/modeling_evaluation.py
@@ -33,7 +33,7 @@ def train_random_forest(X_train, y_train):
 def evaluate_classifier(model, X_test, y_test, model_name):
     preds = model.predict(X_test)
 
-    accuracy = accuracy_socre(y_test, preds)
+    accuracy = accuracy_score(y_test, preds)
     report = classification_report(y_test, preds)
 
     print(f'\n(model_name) performance')
