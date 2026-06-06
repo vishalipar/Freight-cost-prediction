43f944349cc867cc29725bd309ae1f7b2c93379b
vishalipar <vishalipar04@gmail.com>
2026-06-06 13:26:52 +0530

    Add train python file, generate the models

---
+++ invoice_flagging/train.py
@@ -0,0 +1,36 @@
+from data_preprocessing import load_invoice_data, split_data, scale_features, apply_labels
+from modeling_evaluation import train_random_forest, evaluate_classifier
+import joblib
+
+FEATURES = [
+    'invoice_quantity',
+    'invoice_dollars',
+    'Freight',
+    'total_item_quantity',
+    'total_item_dollars'
+]
+
+TARGET = 'flag_invoice'
+
+def main():
+    df = load_invoice_data()
+    df = apply_labels(df)
+
+    X_train, X_test, y_train, y_test = split_data(df, FEATURES, TARGET)
+    X_train_scaled, X_test_scaled = scale_features(
+    X_train, X_test, 'models/scaler.pkl'
+    )
+
+    grid_search = train_random_forest(X_train_scaled, y_train)
+
+    evaluate_classifier(
+        grid_search.best_estimator_,
+        X_test_scaled,
+        y_test,
+        'Random Forest Classifier'
+    )
+
+    joblib.dump(grid_search.best_estimator_, 'models/predict_flag_invoice.pkl')
+
+if __name__ == '__main__':
+    main()
