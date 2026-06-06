06e6ccfcd517907be3512f701a719a6e50233a33
vishalipar <vishalipar04@gmail.com>
2026-06-06 16:08:06 +0530

    Add predict invoice flag python file

---
+++ inference/predict_invoice_flag.py
@@ -0,0 +1,34 @@
+import joblib
+import pandas as pd
+
+MODEL_PATH = '../models/predict_flag_invoice.pkl'
+
+def load_model(model_path: str=MODEL_PATH):
+    """
+    Load trained freight cost prediction model.
+    """
+    with open(model_path, 'rb') as f:
+        model= joblib.load(f)
+    return model
+
+def predict_invoice_flag(input_data):
+    """
+    Predict freight cost for new vondor invoices.
+
+    Parameters
+    input data: dict
+
+    Returns
+    pd.DataFrame with predicted freight cost
+    """
+    model = load_model()
+    input_df = pd.DataFrame(input_data)
+    input_df['Predicted_Flag'] = model.predict(input_df).round()
+    return input_df
+
+if __name__ == '__main__':
+    sample_data = {
+        'Dollars':[18500, 9000, 3000, 200]
+    }
+    prediction = predict_invoice_flag(sample_data)
+    print(prediction)
